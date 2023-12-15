06b06676358c196e4107028b77c7be3ba84b694e
Jose RM <mojica.raul@gmail.com>
2023-12-15 00:01:03 -0600

    Created the testing for the Post, Like, and Comment  classes with Rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,9 +20,32 @@ Rspec.describe User, type: :model do
       expect(subject).not_to be_valid
     end
 
+    it 'is invalid when post_counter is not an integer' do
+      subject.post_counter = 'one'
+      expect(subject).not_to be_valid
+    end
+
     it 'is invalid when post_counter is less than 0' do
       subject.post_counter = -1
       expect(subject).not_to be_valid
+    end
+  end
+
+  describe '#recent_posts method check' do
+    before do
+      5.times do |index|
+        Post.create(
+          title: "Post #{index}",
+          text: "Text post #{index}",
+          author: subject,
+          comments_counter: 0,
+          likes_counter: 0
+        )
+      end
+    end
+
+    it 'should return the most recent posts' do
+      expect(subject.recent_posts.length).to eq(3)
     end
   end
 end
