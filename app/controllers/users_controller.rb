642d4f844c2e8a8568057d414917bd031b60300a
Jose RM <mojica.raul@gmail.com>
2023-12-20 22:53:24 -0600

    Fixed issues in the show.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: param[:id])
+    @user = User.find_by(id: params[:id])
     @recent_posts = @user.recent_posts
   end
 end
