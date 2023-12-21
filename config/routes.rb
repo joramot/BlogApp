81d47d614d3244d3945b0ae6cf092da477747aab
Jose RM <mojica.raul@gmail.com>
2023-12-21 16:58:27 -0600

    Fixed issues in the routes file, add the actions 'new' and 'create' to posts route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     resources :users, only: [:index, :show] do
-      resources :posts, only: [:index, :new, :create :show] do
+      resources :posts, only: [:index, :new, :create, :show] do
         resources :comments, only: [:create, :new]
         resources :likes, only: [:create]
       end
     end
-    resources :posts, only [:create]
+    resources :posts, only: [:create]
     root "users#index"
   # Defines the root path route ("/")
   # root "articles#index"
