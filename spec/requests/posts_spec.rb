951ae117110719c13baf61ec9a1aaa12213a0f9b
Jose RM <mojica.raul@gmail.com>
2024-01-15 20:21:21 -0600

    Fixed issues in the definition of the testing files of the post_spec and users_spec.

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 describe 'Posts', type: :request do
+  let!(:user) { User.create(name: 'Emi', photo: 'https://picsum.photos/100', bio: 'Football player') }
+  let!(:post) do
+    Post.create(author_id: user.id, title: 'My life in football',
+                text: 'I am a player in the youth category for the Linces FBA team in the receiver position.')
+  end
   describe 'GET #index' do
     before :each do
-      get '/users/1/posts'
+      get user_posts_path(user.id)
     end
 
     # If response status was correct.
@@ -18,13 +23,13 @@ describe 'Posts', type: :request do
 
     # If the response body includes correct placeholder text.
     it 'check if the response boby includes the correct placeholder text' do
-      expect(response.body).to include('Posts')
+      expect(response.body).to include('My life in football')
     end
   end
 
   describe 'GET #show each user posts' do
     before :each do
-      get '/users/1/posts/1'
+      get user_post_path(post.author_id, post.id)
     end
 
     # If response status was correct.
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 describe 'Users', type: :request do
+  let!(:user) { User.create(name: 'Peter', photo: 'https://picsum.photos/100', bio: 'Football player') }
+  let!(:post) do
+    Post.create(title: 'My life in football', text: 'I am a player in the youth category for the Linces FBA team.',
+                author_id: user.id)
+  end
+
   describe 'GET #index' do
     before :each do
       get users_path
@@ -18,13 +24,13 @@ describe 'Users', type: :request do
 
     # If the response body includes correct placeholder text.
     it 'check if the response boby includes the correct placeholder text' do
-      expect(response.body).to include('Users')
+      expect(response.body).to include('Peter')
     end
   end
 
   describe 'GET #show' do
     before :each do
-      get user_path(1)
+      get user_path(user.id)
     end
 
     # If response status was correct.
