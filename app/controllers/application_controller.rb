b1b59664f569f0c438199a71fb3d54762fa58442
Jose RM <mojica.raul@gmail.com>
2024-01-17 22:12:55 -0600

    Installed devise gem, create controllers to devise, created model to user, created new migration to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    @current_user ||= User.first
-  end
-  helper_method :current_user
+  # def current_user
+  #  @current_user ||= User.first
+  # end
+  # helper_method :current_user
+
+
 end
