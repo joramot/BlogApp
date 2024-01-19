a58f9673d0439b786c8deb44de233381501e0bfc
Jose RM <mojica.raul@gmail.com>
2024-01-19 15:31:13 -0600

    Fixed the second param in  the method user_post_path. add .id to post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,10 +9,16 @@ class CommentsController < ApplicationController
     @comment = @post.comments.new(user: current_user, **comment_params)
 
     if @comment.save
-      redirect_to user_post_path(@post.author, @post), notice: 'Comment Created Successfully'
+      redirect_to user_post_path(@post.author_id, @post.id), notice: 'Comment Created Successfully' # agrege el id a post en el segundo argumentp de redirect_to
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to user_post_path(@comment.post.author, @comment.post), notice: 'Comment was successfully Deleted.'
   end
 
   private
