334243b9c9500a4b6a1c7f0633f5eb13c9fca409
Jose RM <mojica.raul@gmail.com>
2024-01-19 15:28:40 -0600

    Delete one innecesary line of comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
-  # def current_user
-  #  @current_user ||= User.first
-  # end
-  # helper_method :current_user
 
   before_action :authenticate_user!
 
